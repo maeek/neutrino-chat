8aef0bda931f9c988325828ed528ae280c82d0fc
maeek <maciek299.ms@gmail.com>
2021-03-07 20:19:16 +0100

    test: Updated setupTests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { DateTime } from 'luxon';
+
+export const TIME_MOCK = '2021-01-30T18:05:38.652+01:00' as unknown as DateTime;
+
+jest.mock('luxon', () => ({
+  DateTime: {
+    local: () => TIME_MOCK
+  }
+}));
