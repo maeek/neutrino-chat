4c7145627158d92928c5fcf2cfbfebc03fcbcbf8
maeek <maciek299.ms@gmail.com>
2021-03-12 21:54:10 +0100

    build: Added bundle analyzer

---
+++ scripts/analyze.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-extraneous-dependencies */
+process.env.NODE_ENV = 'production';
+
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const webpackConfigProd = require('../config/webpack.config')('production');
+
+const chalk = require('chalk');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
+const green = (text) => chalk.green.bold(text);
+
+// pushing BundleAnalyzerPlugin to plugins array
+webpackConfigProd.plugins.push(new BundleAnalyzerPlugin());
+webpackConfigProd.plugins.push(
+  new ProgressBarPlugin({
+    format: `${green('analyzing...')} ${green('[:bar]')}${green('[:percent]')}${green('[:elapsed seconds]')} - :msg`
+  })
+);
+
+webpack(webpackConfigProd, (err, stats) => {
+  if (err || stats.hasErrors()) {
+    console.error(err);
+  }
+});
