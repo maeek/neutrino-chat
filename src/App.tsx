916a5ce4a51b7ae86f11c9bcdfcdf2a7c8b8589e
maeek <maciek299.ms@gmail.com>
2021-03-12 21:30:38 +0100

    feat: Added service worker

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { Provider } from 'react-redux';
-import store from './store/';
 import Router from './pages/router';
 import './styles/App.scss';
 
-const App = () => (
-  <Provider store={store}>
-    <Router />
-  </Provider>
-);
+const App = () => <Router />;
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './report-web-vitals';
+import * as serviceWorkerRegistration from './register-service-worker';
+import { Provider } from 'react-redux';
+import store from './store/';
+// import remoteStoreWrapper from './store/remote-store-wrapper';
 import './styles/index.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('app-root')
-);
+// if (process.env.EXPERIMENTAL && process.env.NODE_ENV === 'development') {
+//   store = require('./store/store.worker');
+//   store = await remoteStoreWrapper(store as any);
+// }
+
+const run = async () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById('app-root')
+  );
+};
+
+run();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // eslint-disable-next-line no-console
 reportWebVitals(console.log);
+
+
+// Register service worker
+serviceWorkerRegistration.register();
