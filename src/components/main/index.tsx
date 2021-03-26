c9c6f24ddc3df2a65183aa61a35cf1416dc9758e
maeek <maciek299.ms@gmail.com>
2021-03-26 22:28:37 +0100

    feat: Main window

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,8 +1,12 @@
 import { FC } from 'react';
 import { RouteProps } from 'react-router-dom';
 import LayoutContentFooter from '@maeek/neutrino-design/components/layouts/content-footer';
+import LayoutSideContent from '@maeek/neutrino-design/components/layouts/side-content';
 import GenericFooter from '@components/common/footer/generic';
+import MainSearchBar from './search-bar/search-bar';
 import './styles/main.scss';
+import { Heading } from '@maeek/neutrino-design/components/atoms/typography/heading';
+
 interface MainViewProps extends RouteProps {
   [key: string]: any;
 }
@@ -13,7 +17,11 @@ export const MainView: FC<MainViewProps> = () => {
   return (
     <div className="view-root view-root--main">
       <LayoutContentFooter footerNode={footerNode}>
-        Yee
+        <LayoutSideContent className="main-side-filter" sideNode={<div className="nav-placeholder" />}>
+          <MainSearchBar />
+          <Heading level={4} className="main-side-filter-heading">Contacts</Heading>
+          <div style={{margin: '0 1rem 1rem', height: '300px', width: 'auto', background: '#22262b', borderRadius: '4px'}}></div>
+        </LayoutSideContent>
       </LayoutContentFooter>
     </div>
   );
