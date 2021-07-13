8cbb9eb6da754f76193817ea9c95d25d55bc3968
maeek <maciej@suchanecki.me>
2021-07-13 18:37:01 +0200

    feat: top-bar styling

--- src/components/common/top-bar/drawer/button.tsx
+++ src/components/common/top-bar/drawer/button.tsx
@@ -1,9 +1,12 @@
 import {
+  CSSProperties,
   KeyboardEvent,
   useRef
 } from 'react';
+import { useSelector } from 'react-redux';
 import { NotificationsNoneRounded } from '@material-ui/icons';
 import './button.scss';
+import { getMeColor } from '@/selectors/user';
 
 export interface NotificationsDrawerButtonProps {
   onOpen?: () => void;
@@ -11,7 +14,8 @@ export interface NotificationsDrawerButtonProps {
 
 export const NotificationsDrawerButton = ({ onOpen }: NotificationsDrawerButtonProps) => {
   const bellIconRef = useRef<HTMLSpanElement>(null);
-
+  const meColor = useSelector(getMeColor);
+  
   const onEnter = (fn?: () => void) => (e: KeyboardEvent<HTMLSpanElement>) => {
     if (e.key === 'Enter' && fn) {
       fn();
@@ -25,6 +29,7 @@ export const NotificationsDrawerButton = ({ onOpen }: NotificationsDrawerButtonP
       onClick={onOpen}
       onKeyUp={onEnter(onOpen)}
       ref={bellIconRef}
+      style={{ color: meColor } as CSSProperties}
     >
       <NotificationsNoneRounded />
     </span>
