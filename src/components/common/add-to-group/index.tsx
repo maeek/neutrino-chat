8c6a47774def5d712c6aa941ecb4bc7db7307703
maeek <maciej@suchanecki.me>
2021-10-21 16:56:22 +0200

    feat: Updated group add UI

--- src/components/common/add-to-group/index.tsx
+++ src/components/common/add-to-group/index.tsx
@@ -1,13 +1,13 @@
 import { getMeGroupsList } from '@/selectors/groups';
-import { Button, ContextMenu } from '@maeek/neutrino-design';
-import { ContextMenuItems } from '@maeek/neutrino-design/components/molecules/context-menu/Menu';
+import { Button } from '@maeek/neutrino-design/components/atoms/buttons/Button';
+import { ContextMenu, ContextMenuItems } from '@maeek/neutrino-design/components/molecules/context-menu/Menu';
 import { AddRounded, CheckRounded } from '@material-ui/icons';
 import CloseRounded from '@material-ui/icons/CloseRounded';
-import { Text } from '@maeek/neutrino-design/components/atoms/typography/text';
+import { Text } from '@maeek/neutrino-design/components/atoms/typography/text/Text';
 import { useDispatch, useSelector } from 'react-redux';
-import './add-to-group.scss';
 import { Groups, GroupTypeEnum } from '@/store/me/groups/types';
 import { addMembersToGroup, removeMembersFromGroup } from '@/store/me/groups/actions';
+import './add-to-group.scss';
 
 export interface AddToGroupProps {
   item: string;
