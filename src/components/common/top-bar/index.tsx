7185e0e7346ff934401d6a3416db0a84bcb69f19
maeek <maciej@suchanecki.me>
2021-10-21 17:03:39 +0200

    feat: Added top-bar quick links

--- src/components/common/top-bar/index.tsx
+++ src/components/common/top-bar/index.tsx
@@ -1,17 +1,27 @@
-import { memo } from 'react';
+import { memo, MouseEventHandler } from 'react';
 import { useMediaQuery } from 'react-responsive';
+import { useHistory } from 'react-router-dom';
 import NavigationControlls from '@/components/common/navigation-controlls/navigation';
 import { ContextMenuWrapper } from './context-menu/context-menu-wrapper';
 import NotificationsDrawer from './drawer/';
 import { TopBarHeading } from './heading';
-import './top-bar.scss';
 import classNames from 'classnames';
 import { useSelector } from 'react-redux';
-import { isAppUITopBarHidden } from '@/selectors/app-ui';
+import { isAppUITopBarHidden, isAppUITopBarQuickNavHidden } from '@/selectors/app-ui';
+import Text from '@maeek/neutrino-design/components/atoms/typography/text/Text';
+import Navigator from '@/utils/navigation';
+import './top-bar.scss';
 
 export const TopBar = () => {
+  const history = useHistory();
   const isMobile = useMediaQuery({ maxWidth: 768 });
   const isHidden = useSelector(isAppUITopBarHidden);
+  const isQuickNavHidden = useSelector(isAppUITopBarQuickNavHidden);
+
+  const onClickLink = (link: string): MouseEventHandler => (e) => {
+    e.preventDefault();
+    Navigator.forward(history, link);
+  };
 
   return (
     <nav className={classNames('top-bar', !isHidden && 'top-bar--visible')}>
@@ -19,6 +29,13 @@ export const TopBar = () => {
         <div className="top-bar-left">
           <NavigationControlls pwaOnly />
           <TopBarHeading />
+          {
+            !isMobile && !isQuickNavHidden && (
+              <div className="main-chats-navigation">
+                <Text link='/browse' onClick={onClickLink('/browse')}>Browse</Text>
+              </div>
+            )
+          }
         </div>
         <div className="top-bar-right">
           {!isMobile && <NotificationsDrawer />}
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,12 +1,25 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import LayoutContentFooter from '@maeek/neutrino-design/components/layouts/content-footer';
 import LayoutSideContent from '@maeek/neutrino-design/components/layouts/side-content';
 import GenericFooter from '@/components/common/footer/generic';
-import MainSearchBar from './search-bar/search-bar';
+import MainSearchBar from '../common/search-bar/search-bar';
 import UsersCards from './content/users';
 import { SideNav } from './side-nav';
+import { setTopBarQuickNavVisibility } from '@/store/app/ui/actions';
 import './main.scss';
 
 export const MainView = () => {
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(setTopBarQuickNavVisibility(true));
+    
+    return () => {
+      dispatch(setTopBarQuickNavVisibility(false));
+    };
+  }, [ dispatch ]);
+
   const footerNode = <GenericFooter />;
 
   return (
